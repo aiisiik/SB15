a8a8fc1316e0f46869e0fa958d6aaa6ae7e78c2d
aiisiik <ai.sen@mail.ru>
2023-03-25 09:59:17 +0900

    Добавьте файлы проекта.

---
+++ SB15.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+const int N = 100;
+
+int FindOddNumbers(int Limit, bool IsOdd) {
+	for (int i = IsOdd; i <= Limit; i=i+2) {
+		std::cout << i << " ";
+	}
+	return 0;
+}
+
+int main() {
+	FindOddNumbers(N, false);
+}
